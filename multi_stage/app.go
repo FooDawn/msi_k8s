81a554da1058c94438da01abd5145723dccb03df
FooDawn <mal.medo987@gmail.com>
2022-01-24 03:13:56 +0100

    konec testiranja upam

--- multi_stage/app.go
+++ multi_stage/app.go
@@ -9,7 +9,7 @@ import (
 func main() {
     fmt.Println("Hello, World!")
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\nFor more go to I really don't know yet. But try just /", r.URL.Path[1:])
+        fmt.Fprintf(w, "Hello, you've requested to be here.\nBut for more just try /", r.URL.Path[1:])
     })
 
     log.Fatal(http.ListenAndServe(":5002", nil))
